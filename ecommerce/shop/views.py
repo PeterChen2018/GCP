b4edba483105b4b003ccaebd775fbbefcc59a6f1
Peter Chen <twstock99@gmail.com>
2018-09-09 20:20:09 +0800

    peter

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def index(request):
+    now=datetime.now()
+    return render(request,"hi.html",locals()) 
+
+
+
