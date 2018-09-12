faba8c1b10594afd67b052aa5e59abd90aa1e6b3
Peter Chen <twstock99@gmail.com>
2018-09-12 15:21:45 +0800

    peter

---
+++ python27/ecommerce/ecommerce/urls.py
@@ -0,0 +1,61 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+Use url instead of path.
+
+from django.conf.urls import url
+urlpatterns = [
+url('',views.homepageview,name='home')
+]
+
+
+
+from django.contrib import admin
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
+
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from shop.views import product_list
+
+urlpatterns = [
+    # url(r'^$', index),
+	# url('', include('shop.urls')),
+	url(r'^$', product_list ),
+	url(r'^shop/', include('shop.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
---
+++ python27/py27env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\git\gcp\python27\py27env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
